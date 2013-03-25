e40be66572a9a960d6c5bfd029681c2e1037db9d
Matthew Clark <web.development@undp.org>
2013-03-25 14:24:54 +0545

    Removed dsm()

--- sites/all/themes/idap/templates/system-templates/idap-secondary-menu.tpl.php
+++ sites/all/themes/idap/templates/system-templates/idap-secondary-menu.tpl.php
@@ -1,8 +1,6 @@
-
 <ul>
 	<?php foreach($element['menu_tree'] as $item=>$array): ?>
 		<li>
-			<?php dsm($array); ?>
 			<?php print l($array['link']['link_title'], $array['link']['link_path']); ?>
 		</li>
 	<?php endforeach; ?>
