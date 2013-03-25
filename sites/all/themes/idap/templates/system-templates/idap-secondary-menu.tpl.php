b4a357336f2d5954bdf192b698be4743ccc4e690
Matthew Clark <web.development@undp.org>
2013-03-25 14:23:28 +0545

    Added dsm()

--- sites/all/themes/idap/templates/system-templates/idap-secondary-menu.tpl.php
+++ sites/all/themes/idap/templates/system-templates/idap-secondary-menu.tpl.php
@@ -2,7 +2,7 @@
 <ul>
 	<?php foreach($element['menu_tree'] as $item=>$array): ?>
 		<li>
-			dsm($array);
+			<?php dsm($array); ?>
 			<?php print l($array['link']['link_title'], $array['link']['link_path']); ?>
 		</li>
 	<?php endforeach; ?>
