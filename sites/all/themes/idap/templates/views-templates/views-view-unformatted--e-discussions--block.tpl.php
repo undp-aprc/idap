88a4f65f5416de1d1232c8dc4dbd5e636c2f9643
Matthew Clark <web.development@undp.org>
2013-05-23 18:47:31 +0545

    E-Discussion block display system added

---
+++ sites/all/themes/idap/templates/views-templates/views-view-unformatted--e-discussions--block.tpl.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php if (!empty($title)): ?>
+  <h2><?php print $title; ?></h2>
+<?php endif; ?>
+<ul>
+<?php foreach ($rows as $id => $row): ?>
+  <li><?php print $row; ?></li>
+<?php endforeach; ?>
+</ul>
