0a7e684772431b6dd32033664b89ba66d1ae6040
Matthew Clark <web.development@undp.org>
2013-05-23 19:17:01 +0545

    Formatting for e-discussion block

--- sites/all/themes/idap/templates/views-templates/views-view-unformatted--e-discussions--block.tpl.php
+++ sites/all/themes/idap/templates/views-templates/views-view-unformatted--e-discussions--block.tpl.php
@@ -7,11 +7,15 @@
  * @ingroup views_templates
  */
 ?>
+<div class="clearfix"></div>
 <?php if (!empty($title)): ?>
   <h2><?php print $title; ?></h2>
 <?php endif; ?>
+<div class="content block-content-item">
 <ul>
 <?php foreach ($rows as $id => $row): ?>
   <li><?php print $row; ?></li>
 <?php endforeach; ?>
 </ul>
+</div>
+<div class="clearfix"></div>
