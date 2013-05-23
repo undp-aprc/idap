e28dd5ead134e221370d2169a8a735672ad1c288
Matthew Clark <web.development@undp.org>
2013-05-23 19:30:28 +0545

    Added header (manually) to e-discussion block

--- sites/all/themes/idap/templates/views-templates/views-view-unformatted--e-discussions--block.tpl.php
+++ sites/all/themes/idap/templates/views-templates/views-view-unformatted--e-discussions--block.tpl.php
@@ -7,15 +7,11 @@
  * @ingroup views_templates
  */
 ?>
-<div class="clearfix"></div>
 <?php if (!empty($title)): ?>
-  <h2><?php print $title; ?></h2>
+  <h3><?php print $title; ?></h3>
 <?php endif; ?>
-<div class="content block-content-item">
 <ul>
 <?php foreach ($rows as $id => $row): ?>
   <li><?php print $row; ?></li>
 <?php endforeach; ?>
 </ul>
-</div>
-<div class="clearfix"></div>
