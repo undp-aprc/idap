df168786d89b81b9964c810f322614b57081226a
Matthew Clark <web.development@undp.org>
2013-06-12 10:54:49 +0545

    Fixed layout problem with blocking listing content and photo.

--- sites/all/themes/idap/templates/block-templates/block--bean--summary-listing-photo.tpl.php
+++ sites/all/themes/idap/templates/block-templates/block--bean--summary-listing-photo.tpl.php
@@ -1,12 +1,18 @@
-<div class="block-heading">
-	<h2><?php print $title; ?></h2>
-	<div class="clearfix"></div>
-</div>
-<div class="clearfix block-heading-divider"></div>
-<div class="block-main-content block-content summary-listing-photo">
-	<?php print render($bean['photo']); ?>
-	<ul>
-	<?php print $block_content['node_items']; ?>
-	</ul>
-	<div class="clearfix"></div>
+<div class="summary-listing-photo">
+	<div class="block-heading">
+		<h2><?php print $title; ?></h2>
+		<div class="clearfix"></div>
+	</div>
+	<div class="clearfix block-heading-divider"></div>
+	<div class="block-main-content block-content">
+		<div class="photo float-left">
+			<?php print render($bean['photo']); ?>
+		</div>
+		<div class="content float-left">
+			<ul>
+				<?php print $block_content['node_items']; ?>
+			</ul>
+		</div>
+		<div class="clearfix"></div>
+	</div>
 </div>
