70f272a21d0377c0fc8fe3f9d487207167c38948
Matthew Clark <matthew.clark@undp.org>
2013-07-16 13:58:42 +0545

    Added rules module and patched node module with https://drupal.org/files/node-access-records-1146244-110.patch ref: https://drupal.org/node/1146244#comment-7290062

--- sites/all/modules/idap_custom_modules/idap_custom_forms/templates/e-discussion-node-form.tpl.php
+++ sites/all/modules/idap_custom_modules/idap_custom_forms/templates/e-discussion-node-form.tpl.php
@@ -1,6 +1,6 @@
 <div style="height:20px;"></div>
 <div class="e-discussion-node-add-wrapper e-discussion-node-form">
-  	<div class="node-column-main equal-column-heights">
+  	<div class="node-column-main">
 		<div class="enclosure">
     	<?php if($form): ?>
       		<?php print drupal_render_children($form); ?>
@@ -12,7 +12,7 @@
 	  	<?php endif; ?>
 		</div>
   	</div>
-  	<div class="node-column-sidebar highlight equal-column-heights">
+  	<div class="node-column-sidebar highlight">
 		<div class="enclosure">
 		<h3>Content Audience Options</h3>
 		<p class="dark-grey"><?php print t('By using the options below you can choose where your content will be displayed within this site\'s sub-theme pages, block and groups.'); ?></p>
