a35ef1f8cdd74e73c70208f51f243ac65a79212e
Matthew Clark <matthew.clark@undp.org>
2013-07-22 14:18:17 +0545

    Fixed HTML template for emails

--- sites/all/themes/idap_mail/templates/page.tpl.php
+++ sites/all/themes/idap_mail/templates/page.tpl.php
@@ -83,20 +83,18 @@
 <td width="77" rowspan="2"><a href="http://www.undp.org" target="_blank"><img src="http://idap:8888/sites/all/themes/idap/images/logos/undp.png" alt="UNDP Logo" style="border:0px;"/></a></td>
 <td width="25"><a href="http://www.youtube.com" target="_blank"><img src="http://idap:8888/sites/all/themes/idap/images/icons/youtube.png" alt="Youtube" border="0" /></a></td>
 <td width="25"><a href="http://www.facebook.com" target="_blank"><img src="http://idap:8888/sites/all/themes/idap/images/icons/facebook.png" alt="Facebook" border="0" /></a></td>
-<td width="100" valign="top"><a href="http://www.twitter.com" target="_blank"><img src="http://idap:8888/sites/all/themes/idap/images/icons/twitter.png" alt="Twitter" border="0" /></a></td>
-<td width="367" valign="top"></td>
+<td width="25" valign="top"><a href="http://www.twitter.com" target="_blank"><img src="http://idap:8888/sites/all/themes/idap/images/icons/twitter.png" alt="Twitter" border="0" /></a></td>
+<td width="367" valign="top" colspan="4"></td>
 </tr>
 <tr>
-<td colspan="4" valign="top"><a href="http://www.inclusivedevelopmentasiapacific.net" target="_blank"><img src="http://idap:8888/sites/all/themes/idap/images/logos/logo.png" alt="Site Logo" border="0" /></a></td>
+<td colspan="7" valign="top"><a href="http://www.inclusivedevelopmentasiapacific.net" target="_blank"><img src="http://idap:8888/sites/all/themes/idap/images/logos/logo.png" alt="Site Logo" border="0" /></a></td>
 </tr>
-<tr><td colspan="5">
+<tr><td colspan="8">
 <div style="margin-top:10px;padding-top:10px;border-top:1px solid #cccccc;"></div>
 <?php if ($page['content']): ?>
 <?php print render($page['content']); ?>
 <?php endif; ?>
 </td></tr>
-</table>
-<table width="800">
 <tr style="background-color:#EEBC5F;">
   	<td width="480" colspan="4" style="font-size:0.7em;">&copy; United Nations Development Programme 2012</td>
     <td width="80"><a title="About Inclusive Development Asia-Pacific" href="http://www.inclusivedevelopmentasiapacific.net/about-us" style="text-decoration:none;color:#000;font-size:0.7em;">About Us</a></td>
@@ -104,13 +102,17 @@
     <td width="80"><a title="Information about the privacy policy that governs how IDAP may use personal information collected through this site." href="http://www.inclusivedevelopmentasiapacific.net/privacy" style="text-decoration:none;color:#000;font-size:0.7em;">Privacy Statement</a></td>
     <td width="80"><a title="Terms and Conditions for membership, content creation and sharing." href="http://www.inclusivedevelopmentasiapacific.net/terms" style="text-decoration:none;color:#000;font-size:0.7em;">Terms &amp; Conditions</a></td>
   </tr>
-</table>
-<table width="800">
-  <tr style="background-color:#EAEAEB"><td>
+  <tr style="background-color:#EAEAEB"><td colspan="8">
   <address style="font-style:normal;font-size:0.7em">
   <b>The United Nations Development Programme (Asia Pacific Regional Center)</b>
-  Level 3 Service Building
+  </address>
+  <address style="font-style:normal;font-size:0.7em">
+  Level 3 UN Service Building
+  </address>
+  <address style="font-style:normal;font-size:0.7em">
   Ratchadamnoen Nok Avenue
+  </address>
+  <address style="font-style:normal;font-size:0.7em">
   Bangkok 10300 Thailand
   </address>
   </td></tr>
